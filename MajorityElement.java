30f3969b3daa4662d4af1c6eca85ebaa7e27be3e
Aryan Raj <nr023018@gmail.com>
2026-08-06 22:05:28 +0530

    Add Majority Element solution in Java
    Added the Java solution for the Majority Element problem using the Boyer-Moore Voting Algorithm. The solution efficiently identifies the majority element by maintaining a candidate and count while traversing the array once. It achieves O(n) time complexity and O(1) extra space. This solution is part of my Java Data Structures and Algorithms (DSA) practice and LeetCode problem-solving journey.

---
+++ MajorityElement.java
@@ -0,0 +1,34 @@
+public class MajorityElement {
+
+    public int majorityElement(int[] nums) {
+
+        int candidate = 0;
+        int count = 0;
+
+        for (int num : nums) {
+
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return candidate;
+    }
+
+    public static void main(String[] args) {
+
+        MajorityElement obj = new MajorityElement();
+
+        int[] nums = {2, 2, 1, 1, 1, 2, 2};
+
+        int result = obj.majorityElement(nums);
+
+        System.out.println(result);
+    }
+}
