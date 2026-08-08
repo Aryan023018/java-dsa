a5f5d46aab19663def64654ffbf3d9773b898a6c
Aryan Raj <nr023018@gmail.com>
2026-08-08 23:03:45 +0530

    Add Remove Element solution in Java
    Added the Java solution for the Remove Element problem using the Two Pointer technique. The solution removes all occurrences of a given value from an array in-place and returns the number of remaining elements. It runs in O(n) time complexity with O(1) extra space. This solution is part of my Java Data Structures and Algorithms (DSA) practice and LeetCode problem-solving journey.

---
+++ RemoveElement.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+
+public class RemoveElement {
+
+    public int removeElement(int[] nums, int val) {
+
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+
+    public static void main(String[] args) {
+
+        RemoveElement obj = new RemoveElement();
+
+        int[] nums = {3, 2, 2, 3};
+        int val = 3;
+
+        int k = obj.removeElement(nums, val);
+
+        System.out.println("Number of remaining elements: " + k);
+
+        System.out.println("Array after removing " + val + ": "
+                + Arrays.toString(Arrays.copyOf(nums, k)));
+    }
+}
