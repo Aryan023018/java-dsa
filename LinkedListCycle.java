620c67f7fc3085f1256f3ee653162de269f2cf0a
Aryan Raj <nr023018@gmail.com>
2026-08-11 20:52:41 +0530

    Add Linked List Cycle solution in Java
    Added the Java solution for the Linked List Cycle problem using Floyd's Cycle Detection Algorithm. The solution uses slow and fast pointers to efficiently detect whether a cycle exists in a linked list. It runs in O(n) time complexity with O(1) extra space. This solution is part of my Java Data Structures and Algorithms (DSA) practice and LeetCode problem-solving journey.

---
+++ LinkedListCycle.java
@@ -0,0 +1,51 @@
+public class LinkedListCycle {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public boolean hasCycle(ListNode head) {
+
+        if (head == null || head.next == null) {
+            return false;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        LinkedListCycle obj = new LinkedListCycle();
+
+        ListNode head = new ListNode(3);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(0);
+        head.next.next.next = new ListNode(-4);
+
+        // Creating a cycle:
+        head.next.next.next.next = head.next;
+
+        boolean result = obj.hasCycle(head);
+
+        System.out.println("Linked List contains a cycle: " + result);
+    }
+}
