dab2ccdb76ce1c72583ea6fccea7190a8b476dc2
Aryan Raj <nr023018@gmail.com>
2026-09-15 07:58:46 +0530

    Add Pow(x, n) solution in Java
    Added the Java solution for LeetCode 50 - Pow(x, n).
    
    The solution uses Binary Exponentiation to calculate x raised to the power n efficiently. It handles zero, positive, and negative powers and uses a long variable to safely handle the minimum integer value.
    
    Time Complexity: O(log n)
    Space Complexity: O(1)
    
    This solution is part of my Java Data Structures and Algorithms (DSA) and LeetCode practice.

---
+++ PowXN.java
@@ -0,0 +1,43 @@
+public class PowXN {
+
+    public double myPow(double x, int n) {
+
+        if (n == 0) return 1.0;
+        if (x == 0) return 0.0;
+        if (x == 1) return 1.0;
+        if (x == -1 && n % 2 != 0) return -1.0;
+
+        long binForm = n;
+
+        if (n < 0) {
+            x = 1 / x;
+            binForm = -binForm;
+        }
+
+        double ans = 1.0;
+
+        while (binForm > 0) {
+
+            if (binForm % 2 == 1) {
+                ans *= x;
+            }
+
+            x *= x;
+            binForm /= 2;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) {
+
+        PowXN obj = new PowXN();
+
+        double x = 2.0;
+        int n = 10;
+
+        double result = obj.myPow(x, n);
+
+        System.out.println("Answer: " + result);
+    }
+}
