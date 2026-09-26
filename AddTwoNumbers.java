70afef343986b0c722b9e7ed27795e8a763a62fc
Aryan Raj <nr023018@gmail.com>
2026-09-26 22:26:23 +0530

    Add Two Numbers solution in Java
    Added Java solution for LeetCode 2 - Add Two Numbers using linked lists and carry handling. The solution processes both lists in O(max(m,n)) time.

---
+++ AddTwoNumbers.java
@@ -0,0 +1,77 @@
+public class AddTwoNumbers {
+
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0) {
+
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+
+            int sum = val1 + val2 + carry;
+
+            carry = sum / 10;
+            int digit = sum % 10;
+
+            current.next = new ListNode(digit);
+            current = current.next;
+
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+
+        return dummy.next;
+    }
+
+    public void printList(ListNode head) {
+
+        while (head != null) {
+            System.out.print(head.val);
+
+            if (head.next != null) {
+                System.out.print(" -> ");
+            }
+
+            head = head.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        AddTwoNumbers obj = new AddTwoNumbers();
+
+        ListNode l1 = new ListNode(2);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(3);
+
+        ListNode l2 = new ListNode(5);
+        l2.next = new ListNode(6);
+        l2.next.next = new ListNode(4);
+
+        ListNode result = obj.addTwoNumbers(l1, l2);
+
+        System.out.print("Result: ");
+        obj.printList(result);
+    }
+}
